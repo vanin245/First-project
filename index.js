a0c86ebc0a23ce47e54edb1908be7dd4a62b623c
andreiej <andrej.a.vanin@gmail.com>
2022-02-06 12:36:06 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const clients = [{
+        id: 1,
+        name: 'Anastasia',
+        lastName: 'Kuznetsova',
+    },
+    {
+        id: 2,
+        name: 'Anastasia',
+        lastName: 'Kuznetsova',
+    },
+
+    {
+        id: 3,
+        name: 'Anastasia',
+        lastName: 'Kuznetsova',
+    },
+    {
+        id: 4,
+        name: 'Anastasia',
+        lastName: 'Kuznetsova',
+    },
+    {
+        id: 5,
+        name: 'Anastasia',
+        lastName: 'Kuznetsova',
+    },
+    {
+        id: 6,
+        name: 'Anastasia',
+        lastName: 'Kuznetsova',
+    },
+    {
+        id: 7,
+        name: 'Anastasia',
+        lastName: 'Kuznetsova',
+    },
+    {
+        id: 8,
+        name: 'Anastasia',
+        lastName: 'Kuznetsova',
+    },
+    {
+        id: 9,
+        name: 'Anastasia',
+        lastName: 'Kuznetsova',
+    },
+    {
+        id: 10,
+        name: 'Anastasia',
+        lastName: 'Kuznetsova',
+    },
+    {
+        id: 11,
+        name: 'Anastasia',
+        lastName: 'Kuznetsova',
+    },
+    {
+        id: 12,
+        name: 'Anastasia',
+        lastName: 'Kuznetsova',
+    },
+]
